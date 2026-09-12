397de8c4ffac9b8dc38b746c8cc25898055e5c39
Rahul Vimal <155147272+rvking47@users.noreply.github.com>
2026-09-12 17:20:51 +0530

    Polish services cards

--- components/Services.jsx
+++ components/Services.jsx
@@ -41,7 +41,7 @@ const Services = () => {
                 <motion.div 
                 whileHover={{scale: 1.05}}
                 key={index}
-                className='border border-gray-400 rounded-lg px-6 sm:px-8 py-10 hover:shadow-black cursor-pointer hover:bg-lightHover hover:-translate-y-1 duration-500 dark:hover:bg-darkHover dark:hover:shadow-white'>
+                className='border border-gray-300 rounded-lg px-6 sm:px-8 py-10 hover:shadow-black cursor-pointer hover:bg-lightHover hover:-translate-y-2 duration-500 dark:border-white/20 dark:hover:bg-darkHover dark:hover:shadow-white'>
                     <Image src={icon} alt='' className='w-10'/>
                     <h3 className='text-lg my-4 text-gray-700 dark:text-white'>{title}</h3>
                     <p className='text-sm text-gray-600 leading-5 dark:text-white/80'>

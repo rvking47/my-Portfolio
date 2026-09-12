f433f67eaf08dcac090ef2c2f725b217fb6d09c9
Rahul Vimal <155147272+rvking47@users.noreply.github.com>
2026-09-12 17:01:19 +0530

    Update footer links and copyright

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -8,17 +8,17 @@ const Footer = ({isDarkMode}) => {
       <div className='text-center'>
         <Image src={isDarkMode ? assets.logo_dark : assets.logo} alt='' className='w-36 mx-auto mb-2'/>
 
-        <div className='w-max flex items-center gap-2 mx-auto'>
+        <div className='w-max max-w-full flex items-center gap-2 mx-auto'>
             <Image src={isDarkMode ? assets.mail_icon_dark : assets.mail_icon} alt='' className='w-6'/>
             rvimal358@gmail.com
         </div>
       </div>
 
     <div className='text-center sm:flex items-center justify-between border-t border-gray-400 mx-[10%] mt-12 py-6'>
-        <p>© 2025 GreatStack. All rights reserved.</p>
+        <p>© 2026 Rahul Vimal. All rights reserved.</p>
         <ul className='flex items-center gap-10 justify-center mt-4 sm:mt-0'>
             <li><a target='_blank' href="https://github.com/rvking47">GitHub</a></li>
-            <li><a target='_blank' href="https://www.linkedin.com/in/rahul09198/">LinkedIn</a></li>
+            <li><a target='_blank' href="https://www.linkedin.com/in/rahul09123/">LinkedIn</a></li>
         </ul>
     </div>
 
